3f1e8a4a10b8c6fd64fc7ddeb6c000efe5a1eaf5
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2019-01-12 16:28:19 +0100

    Play position with two bowls

---
+++ src/bin/position2.rs
@@ -0,0 +1,17 @@
+extern crate mancala;
+
+use mancala::game::Position;
+
+fn main() {
+    for stones in 1..100 {
+        let mut position = Position::from([stones; 2]);
+
+        let mut round = 0;
+        while !position.finished() {
+            position = position.play(0).unwrap();
+            round += 1;
+        }
+
+        println!("stones {}, round {}", stones, round);
+    }
+}
