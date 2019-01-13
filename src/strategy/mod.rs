1e5be5f609701912b6dd86b5b419e8a704500745
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2019-01-13 13:51:48 +0100

    Use Bowl type for strategy

--- src/strategy/mod.rs
+++ src/strategy/mod.rs
@@ -1,9 +1,9 @@
 //! Various strategies for playing Mancala
 
-use super::game::Position;
+use super::game::{Position, Bowl};
 
 /// A strategy for playing Mancala
 pub trait Strategy {
     /// Return the play for this position
-    fn play(position: &Position) -> usize;
+    fn play(position: &Position) -> Option<Bowl>;
 }
