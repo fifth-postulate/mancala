931e3dbb99a5c56969a20df4ddb3c985a3fbc5c4
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2019-01-13 14:08:59 +0100

    Implement first naive strategy

--- src/strategy/mod.rs
+++ src/strategy/mod.rs
@@ -1,5 +1,7 @@
 //! Various strategies for playing Mancala
 
+pub mod naive;
+
 use super::game::{Position, Bowl};
 
 /// A strategy for playing Mancala
@@ -7,3 +9,4 @@ pub trait Strategy {
     /// Return the play for this position
     fn play(position: &Position) -> Option<Bowl>;
 }
+
---
+++ src/strategy/naive/mod.rs
@@ -0,0 +1,15 @@
+//! Naive strategies, mainly for testing purposes.
+
+use super::super::game::{Position, Bowl};
+use super::Strategy;
+
+/// Pick the first option.
+pub struct First {}
+
+impl Strategy for First {
+    fn play(position: &Position) -> Option<Bowl> {
+        let options = position.options();
+        options.first().cloned()
+    }
+}
+
