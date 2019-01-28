5cee389ff9e8aab12dcdb8da0089aa71668b064d
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2019-01-28 15:30:51 +0100

    Allow for a larger grace period for Travic CI

--- tests/execution_time_alphabeta.rs
+++ tests/execution_time_alphabeta.rs
@@ -6,7 +6,7 @@ use mancala::bout::Bout;
 use mancala::game::GameBuilder;
 use mancala::strategy::AlphaBeta;
 
-const MAXIMUM_ALLOWED_DURATION: Duration = Duration::from_millis(450);
+const MAXIMUM_ALLOWED_DURATION: Duration = Duration::from_millis(1000);
 
 #[test]
 fn calculating_end_games_should_be_quick() {
