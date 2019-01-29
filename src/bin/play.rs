062dab8fc550466690902611d7fc4621b1ec00f1
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2019-01-29 14:22:31 +0100

    Play a standard mancala game against opponent

--- src/bin/play.rs
+++ src/bin/play.rs
@@ -3,14 +3,15 @@ extern crate mancala;
 use std::ops::Neg;
 use mancala::bout::Bout;
 use mancala::game::{GameBuilder, Player};
+use mancala::strategy::tree::Depth;
 use mancala::strategy::{AlphaBeta, User};
 
 fn main() {
     let mut red_strategy = User {};
-    let mut blue_strategy = AlphaBeta::new();
+    let mut blue_strategy = AlphaBeta::limited_to(Depth::Limit(10));
     let mut bout = Bout::new(&mut red_strategy, &mut blue_strategy);
 
-    let game = GameBuilder::new().bowls(2).stones(6).build();
+    let game = GameBuilder::new().bowls(6).stones(4).build();
     let result = bout.start(game).expect("a finished game with score");
     let mut score = result.score().expect("a defined score");
     if result.turn() != Player::Red {
