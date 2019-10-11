18c357782666f3bace90bf0aea85242d42b63be8
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2019-10-11 20:12:28 +0200

    Use clap in play

--- src/bin/play.rs
+++ src/bin/play.rs
@@ -1,17 +1,50 @@
+extern crate clap;
 extern crate mancala;
 
 use std::ops::Neg;
+use clap::{Arg, App};
 use mancala::bout::Bout;
 use mancala::game::{GameBuilder, Player};
 use mancala::strategy::tree::Depth;
 use mancala::strategy::{AlphaBeta, User};
 
 fn main() {
+    let matches = App::new("Play Mancala")
+                          .version("1.0")
+                          .author("Daan van Berkel <daan.v.berkel.1980@gmail.com>")
+                          .about("Challenge the computer to a game of Mancala")
+                          .arg(Arg::with_name("bowls")
+                               .short("b")
+                               .long("bowls")
+                               .value_name("NUMBER")
+                               .help("the numbers of bowls")
+                               .default_value("6")
+                               .takes_value(true))
+                           .arg(Arg::with_name("stones")
+                               .short("s")
+                               .long("stones")
+                               .value_name("NUMBER")
+                               .help("the numbers of stones per bowl")
+                               .default_value("4")
+                               .takes_value(true))
+                           .arg(Arg::with_name("depth")
+                               .short("d")
+                               .long("depth")
+                               .value_name("NUMBER")
+                               .help("the strength of the computer, higher is stronger")
+                               .default_value("5")
+                               .takes_value(true))
+                         .get_matches();
+
+
     let mut red_strategy = User {};
-    let mut blue_strategy = AlphaBeta::limited_to(Depth::Limit(10));
+    let depth = matches.value_of("depth").unwrap().parse().unwrap_or(5);
+    let mut blue_strategy = AlphaBeta::limited_to(Depth::Limit(depth));
     let mut bout = Bout::new(&mut red_strategy, &mut blue_strategy);
 
-    let game = GameBuilder::new().bowls(6).stones(4).build();
+    let bowls = matches.value_of("bowls").unwrap().parse().unwrap_or(6);
+    let stones = matches.value_of("stones").unwrap().parse().unwrap_or(4);
+    let game = GameBuilder::new().bowls(bowls).stones(stones).build();
     let result = bout.start(game).expect("a finished game with score");
     let mut score = result.score().expect("a defined score");
     if result.turn() != Player::Red {
