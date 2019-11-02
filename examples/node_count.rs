b134b041c5cb4ec165cf967067db930178e62e25
Daan van Berkel <daan.van.berkel@alliander.com>
2019-11-02 07:31:58 +0100

    Determine for a tableaux of values the node count

--- examples/node_count.rs
+++ examples/node_count.rs
@@ -4,10 +4,13 @@ use mancala::strategy::{MinMax, Strategy};
 use mancala::game::Position;
 
 fn main() {
-    let position = Position::new(2, 2);
-    let mut strategy = MinMax::new();
-    strategy.play(&position);
+    for bowls in 1..4 {
+        for stones in 1..4 {
+            let position = Position::new(bowls, stones);
+            let mut strategy = MinMax::new();
+            strategy.play(&position);
 
-    println!("{}", strategy.analyzer)
-
+            println!("bowls: {} stones:{} {}", bowls, stones, strategy.analyzer)
+        }
+    }
 }
